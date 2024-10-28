e18742f7ee5b0142b9dc8522b3e54537f7b22a60
shakeyiab <shakeyiabartell@gmail.com>
2024-10-27 22:58:19 -0400

    created get scores

--- userRoutes/scores.mjs
+++ userRoutes/scores.mjs
@@ -1,7 +1,18 @@
 import express from 'express';
+import { BulkOperationBase } from 'mongodb';
 
 
 const router = express.Router();
-
+ const scores =[{
+ id: cowboys vs giants,
+ score: 3-7,
+ winner:cowboys,},{
+id: Ravens vs Bears ,
+score:5-86,
+winner:cowboys,},
+id:Bears vs Giants,
+score:8-0,
+winner:Bears,}
+]
 
     export default router;
--- userRoutes/teams.mjs
+++ userRoutes/teams.mjs
@@ -2,23 +2,19 @@ import express from 'express';
 
 
 const router = express.Router();
-const users = [
-    {
-      id: 1,
-      name: "Carey",
-      username: "cyare23",
-      email: "cy23@example.com",
-    },
-    {
-      id: 2,
-      name: "Mikoto",
-      username: "Miiko",
-      email: "mikoto_u@example.com",
-    },
-    {
-      id: 3,
-      name: "Ronald",
-      username: "RonRonRon",
-      email: "mronald@example.com",
-    },
-  ];  export default router;
+const playingTeams = [{
+    id:cowboys,
+    rank:3,
+    score:58,
+},{
+    id: Ravens,
+    rank:10,
+    score:0,
+
+
+},
+ {
+    id:Bears,
+    ranks:2,
+    score:5,
+ }]
